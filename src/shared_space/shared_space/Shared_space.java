b8dbe95a1c6b00d029d3d487092448b72bdb1280
Suvink <suvink456@yahoo.com>
2020-07-08 17:11:48 +0530

    Added Login UI

--- src/shared_space/shared_space/Shared_space.java
+++ src/shared_space/shared_space/Shared_space.java
@@ -6,9 +6,14 @@
 package shared_space;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -16,22 +21,16 @@ import javafx.stage.Stage;
  * @author suvinnimnaka
  */
 public class Shared_space extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
